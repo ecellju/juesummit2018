cd3d7aac6ed6bfc25de7d83dd476f8682771f8f9
Daipayan Mukherjee <30791985+Satyr09@users.noreply.github.com>
2018-03-12 22:45:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -780,7 +780,7 @@ body {
 }).call(this);
 </script>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script>
 var i=1;
 	setInterval(function(){ 
